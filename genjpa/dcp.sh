3251987e082721c588e186852359b6eee3579f9d
Byounggwan Lee <lbg@sk.com>
2022-07-25 13:54:34 +0900

    dcp.sh 추가

--- genjpa/dcp.sh
+++ genjpa/dcp.sh
@@ -4,23 +4,25 @@ for a in `find /d/lbg/skims -name "*.java" | grep -v genjpa | grep -v gateway | 
 do 
     echo $a '--------------------------------'
     dir=`dirname $a`
-	file=`basename $a`
+	filename=`basename $a`
 
     case $a in
-        *controller* )  echo cp /d/lbg/genjpa/controller/$filename $dir ;;
-		*entity* )      echo cp /d/lbg/genjpa/domain/entity/$filename $dir ;;
-		*repository* )  echo cp /d/lbg/genjpa/domain/repository/$filename $dir
-		                     listenerfile=`echo $file | sed 's/Repository/Listener/g'`
-						     listenerdir=`echo $dir | sed 's/repository/listener/g'` 
-						     if [ ! -d $listenerdir ]
-							 then 
-						         echo mkdir -p $listenerdir
-								 tmp=`echo $dir | sed 's/repository/service/g'`
-                                 echo mkdir -p $tmp
-                                 tmp=`echo $dir | sed 's/domain\/repository/dto/g'`
-								 echo mkdir -p $tmp								
-						     fi
-						echo cp /d/lbg/genjpa/domain/listener/$listenerfile $listenerdir;;
+        *controller* )  cp /d/lbg/genjpa/controller/$filename $dir ;;
+		*entity* )      cp /d/lbg/genjpa/domain/entity/$filename $dir ;;
+		*repository* )  cp /d/lbg/genjpa/domain/repository/$filename $dir
+		                
+						listenerfile=`echo $filename | sed 's/Repository/Listener/g'`
+						listenerdir=`echo $dir | sed 's/repository/listener/g'` 
+						
+						if [ ! -d $listenerdir ]
+						then 
+						    mkdir -p $listenerdir
+						    tmp=`echo $dir | sed 's/repository/service/g'`
+                            mkdir -p $tmp
+                            tmp=`echo $dir | sed 's/domain\/repository/dto/g'`
+						    mkdir -p $tmp								
+						fi
+						cp /d/lbg/genjpa/domain/listener/$listenerfile $listenerdir;;
 		*) echo error 
 	esac
 	echo 
