1dcbfa195e751c85eb9d1f49462df7561a8401ae
jwy2002 <jwy75@nate.com>
2022-08-04 11:30:57 +0900

    설계 조회, 계약 조회 DTO 추가

--- skimspln/src/main/java/com/skims/dto/PlanInformationDto.java
+++ skimspln/src/main/java/com/skims/dto/PlanInformationDto.java
@@ -162,6 +162,12 @@ public class PlanInformationDto {
         private String injrRnkcd; //--상해급수코드
 
 
+        /**
+         * 관계자관계
+         */
+        private String relpcRelcd; //--관계자관계코드
+
+
         /** 담보 */
         private List<Coverage> coverages;
     }
